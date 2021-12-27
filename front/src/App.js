41b70c1d24fcef0ce5c7e99c3ab6b00086cd3ff9
Amir Akhmetzhanov <tawer321@bk.ru>
2021-12-28 00:30:08 +0400

    add antd, insert layot

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Layout } from 'antd';
 import './App.css';
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <Sider style={{color: 'white'}}>
+                <h1 style={{color: 'white'}}>Книги</h1>
+                <h1 style={{color: 'white'}}>Читатели</h1>
+                <h1 style={{color: 'white'}}>Выдачи</h1>
+            </Sider>
+            <Layout>
+                <Header>Header</Header>
+                <Content>Content</Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </Layout>
+    );
 }
 
 export default App;
