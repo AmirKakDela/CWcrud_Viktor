d553a9120d9aebc3a79790e41cb402fb379aed39
Amir Akhmetzhanov <tawer321@bk.ru>
2021-12-28 22:25:00 +0400

    edit is done seems. need add put book on server

---
+++ front/src/Components/Books/BookForm.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import {useDispatch} from "react-redux";
+import * as yup from "yup";
+import {createBook} from "../../redux/thunkAction";
+import {Formik} from "formik";
+
+const BookForm = (props) => {
+    const dispatch = useDispatch();
+    console.log('BookForm', props.editBook)
+
+    const onSubmit = (e, values) => {
+        e.preventDefault()
+        if (values.name && values.author && values.year) {
+            console.log(values)
+            console.log(props.editBook)
+            if (props.editBook) {
+                console.log('put')
+            } else {
+                dispatch(createBook(values))
+            }
+            props.handleCloseForm();
+        }
+    }
+
+    return (
+        <Formik
+            initialValues={props.editBook ? props.editBook : initialValues}
+            validationSchema={validationSchema}
+            validateOnBlur
+            validateOnChange={false}
+            onSubmit={onSubmit}
+        >
+            {(formik) => {
+                return (
+                    <form className="form" onSubmit={e => onSubmit(e, formik.values)}>
+                        <label>Название книги
+                            <input type="text"
+                                   placeholder="Введите название книги..."
+                                   className="form__input"
+                                   name="name"
+                                   onChange={formik.handleChange}
+                                   onBlur={formik.handleBlur}
+                                   value={formik.values.name}
+                            />
+                        </label>
+                        {formik.errors.name && formik.touched.name &&
+                            <p className="form__error-text">{formik.errors.name}</p>}
+
+                        <label> Автор
+                            <input type="text"
+                                   placeholder="Введите автора..."
+                                   className="form__input"
+                                   name="author"
+                                   onChange={formik.handleChange}
+                                   onBlur={formik.handleBlur}
+                                   value={formik.values.author}/>
+                        </label>
+                        {formik.errors.author && formik.touched.author &&
+                            <p className="form__error-text">{formik.errors.author}</p>}
+
+                        <label>Год выпуска
+                            <input type="number"
+                                   placeholder="Введите год выпуска..."
+                                   className="form__input"
+                                   name="year"
+                                   onChange={formik.handleChange}
+                                   onBlur={formik.handleBlur}
+                                   value={formik.values.year}/>
+                        </label>
+                        {formik.errors.year && formik.touched.year &&
+                            <p className="form__error-text">{formik.errors.year}</p>}
+
+                        <label> Количество книг
+                            <input type="number"
+                                   placeholder="Введите количество книг..."
+                                   className="form__input"
+                                   name="count"
+                                   onChange={formik.handleChange}
+                                   onBlur={formik.handleBlur}
+                                   value={formik.values.count}/>
+                        </label>
+                        {formik.errors.count && formik.touched.count &&
+                            <p className="form__error-text">{formik.errors.count}</p>}
+
+                        <button className="button"
+                                type="submit"
+                                disabled={!formik.isValid}
+                        >Сохранить
+                        </button>
+                    </form>
+                )
+            }}
+        </Formik>
+    )
+}
+
+export default BookForm;
+
+
+const validationSchema = yup.object().shape({
+    name: yup.string().required('Это обязательное поле'),
+    author: yup.string().required('Это обязательное поле'),
+    year: yup.number().required('Это обязательное поле')
+        .positive('Год выпуска не может быть отрицательным')
+        .integer().max(2021, 'Год выпуска не может быть больше, чем нынешний год'),
+    count: yup.number().required('Это обязательное поле').positive('Количество не может быть отрицательным').integer()
+})
+
+const initialValues = {
+    name: '',
+    author: '',
+    year: '',
+    count: '',
+}
--- front/src/Components/Books/Books.jsx
+++ front/src/Components/Books/Books.jsx
@@ -1,29 +1,35 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import './books.scss'
 import {useDispatch, useSelector} from "react-redux";
-import {createBook, deleteBook, getBooks} from "../../redux/thunkAction";
+import {deleteBook, getBooks} from "../../redux/thunkAction";
 import {DeleteOutlined, EditOutlined} from '@ant-design/icons';
-import {Formik} from 'formik';
-import * as yup from "yup";
+import BookForm from "./BookForm";
 
 
 const Books = () => {
     const dispatch = useDispatch();
     const books = useSelector(state => state.books.books)
-    const [bookForEdit, setBookForEdit] = useState({})
     const [lookForm, setLookForm] = useState(false);
+    const [editBook, setEditBook] = useState(null)
+    console.log('Book', editBook)
 
     const handleEditBook = (id) => {
         setLookForm(true)
         const book = books.find(book => book._id === id)
-        setBookForEdit(book)
+        setEditBook(book)
     }
 
     const handleCloseForm = () => {
         setLookForm(false)
+        setEditBook(null)
     }
 
-    const handleDeleteBook =  useCallback((id) => {
+    const handleOpenForm = () => {
+        setLookForm(prev => !prev)
+        setEditBook(null)
+    }
+
+    const handleDeleteBook = useCallback((id) => {
         dispatch(deleteBook(id))
     }, [])
 
@@ -35,11 +41,11 @@ const Books = () => {
     return (
         <div>
             <button className="button"
-                    onClick={() => setLookForm(prev => !prev)}
+                    onClick={handleOpenForm}
             >{lookForm ? 'Отменить' : 'Добавить книгу'}</button>
             {lookForm ?
                 <BookForm handleCloseForm={handleCloseForm}
-                          bookForEdit={bookForEdit}
+                          editBook={editBook}
                 />
                 : null}
 
@@ -75,102 +81,3 @@ const Books = () => {
 export default Books;
 
 
-const BookForm = (props) => {
-    const dispatch = useDispatch();
-    const validationSchema = yup.object().shape({
-        name: yup.string().required('Это обязательное поле'),
-        author: yup.string().required('Это обязательное поле'),
-        year: yup.number().required('Это обязательное поле')
-            .positive('Год выпуска не может быть отрицательным')
-            .integer().max(2021, 'Год выпуска не может быть больше, чем нынешний год'),
-        count: yup.number().required('Это обязательное поле').positive('Количество не может быть отрицательным').integer()
-    })
-
-    console.log(props.bookForEdit)
-
-    const initialValues = props.bookForEdit ? props.bookForEdit : {
-        name: '',
-        author: '',
-        year: '',
-        count: '',
-    }
-
-    const onSubmit = (e, values) => {
-        e.preventDefault()
-        if (values.name && values.author && values.year) {
-            dispatch(createBook(values))
-            props.handleCloseForm();
-        }
-    }
-
-    return (
-        <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            validateOnBlur
-            validateOnChange={false}
-            onSubmit={onSubmit}
-        >
-            {(formik) => {
-                return (
-                    <form className="form" onSubmit={e => onSubmit(e, formik.values)}>
-                        <label>Название книги
-                            <input type="text"
-                                   placeholder="Введите название книги..."
-                                   className="form__input"
-                                   name="name"
-                                   onChange={formik.handleChange}
-                                   onBlur={formik.handleBlur}
-                                   value={formik.values.name}
-                            />
-                        </label>
-                        {formik.errors.name && formik.touched.name &&
-                            <p className="form__error-text">{formik.errors.name}</p>}
-
-                        <label> Автор
-                            <input type="text"
-                                   placeholder="Введите автора..."
-                                   className="form__input"
-                                   name="author"
-                                   onChange={formik.handleChange}
-                                   onBlur={formik.handleBlur}
-                                   value={formik.values.author}/>
-                        </label>
-                        {formik.errors.author && formik.touched.author &&
-                            <p className="form__error-text">{formik.errors.author}</p>}
-
-                        <label>Год выпуска
-                            <input type="number"
-                                   placeholder="Введите год выпуска..."
-                                   className="form__input"
-                                   name="year"
-                                   onChange={formik.handleChange}
-                                   onBlur={formik.handleBlur}
-                                   value={formik.values.year}/>
-                        </label>
-                        {formik.errors.year && formik.touched.year &&
-                            <p className="form__error-text">{formik.errors.year}</p>}
-
-                        <label> Количество книг
-                            <input type="number"
-                                   placeholder="Введите количество книг..."
-                                   className="form__input"
-                                   name="count"
-                                   onChange={formik.handleChange}
-                                   onBlur={formik.handleBlur}
-                                   value={formik.values.count}/>
-                        </label>
-                        {formik.errors.count && formik.touched.count &&
-                            <p className="form__error-text">{formik.errors.count}</p>}
-
-                        <button className="button"
-                                type="submit"
-                                disabled={!formik.isValid}
-                        >Сохранить
-                        </button>
-                    </form>
-                )
-            }}
-        </Formik>
-    )
-}
